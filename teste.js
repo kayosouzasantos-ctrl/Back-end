89c57d605d638c028e740dfe4fdb82c304270c0e
kayosouzasantos-ctrl <kayo.souza.santos@escola.pr.gov.br>
2026-04-08 11:06:27 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,25 @@
+// 1. Exiba a mensagem "Olá, mundo" no console.
+console.log("Olá mundo");
+// 2. Crie uma váriavel com seu nome e exiba o valor no console
+let nome = Kayo;
+console.log(nome);
+// 3. Some 2 números e exiba o resultado
+let soma = 1+1;
+console.log(soma);
+// 4. Subtraia 2 números e exiba o resultado
+let subtração = 1-1;
+console.log(subtração);
+// 5. Multiplique dois números e exiba o resultado
+let multiplicação = 1 * 67;
+console.log(multiplicação);
+// 6. Descubra o resto da divisão de um númerp por outro
+let resto = 5 % 1 
+console.log(resto);
+// 7. Crie uma váriavel booleana (true e false) e exiba o seu valor.
+let oIsaacTaCopiandoEu = true;
+console.log = (oIsaacTaCopiandoEu)
+let oIsaacNaoCopia = false
+console.log = (oIsaacTaCopiandoEu)
+// 8. Verifique se um numero é maior que outro
+let diferença = 8 > 6
+console.log = diferença
